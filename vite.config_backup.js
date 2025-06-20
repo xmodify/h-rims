7dbc4e8507b948cc324e104c1b0c1fe5e8af2e36
xmodify <xmodifya@gmail.com>
2025-06-20 18:08:43 +0700

    update

---
+++ vite.config_backup.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    base: '/h-rims/public/',
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});

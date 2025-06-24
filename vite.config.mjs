9dfcec6ec7aa935ec59d069a0abc00a32fb687cd
xmodify <xmodifya@gmail.com>
2025-06-24 22:19:12 +0700

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/h-rims/',
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  base: '/h-rims/',  // ถ้าต้องการตั้ง base path สำหรับโปรเจกต์ (แก้ตามจริง)
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,  // เปิด live reload เวลาแก้ไฟล์ Laravel blade / PHP
+    }),
+  ],
 });
-

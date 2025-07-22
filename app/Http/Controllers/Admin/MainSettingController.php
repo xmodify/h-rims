1bf3c0ee28b0074ea37af5c9a4f51c0c537aacec
xmodify <xmodifya@gmail.com>
2025-07-22 16:56:40 +0700

    update

--- app/Http/Controllers/Admin/MainSettingController.php
+++ app/Http/Controllers/Admin/MainSettingController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 use App\Models\MainSetting;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class MainSettingController extends Controller
 {
 
     public function index()
     {
-        $notify_summary=route('notify_summary');
-        $data = MainSetting::all();
+        $notify_summary=route('notify_summary');       
+        $data = MainSetting::orderBy('id', 'asc')->get();
         return view('admin.main_setting', compact('data','notify_summary'));
     }
 // Update Table main_setting------------------------------------------------------------------------------
@@ -34,56 +35,66 @@ class MainSettingController extends Controller
 // UP Structure -----------------------------------------------------------------------------------------------------------------------    
     public function up_structure(Request $request)
     {
-    //Table main_setting-------------------------------------------------------------------------------------------------------------
-        $main_setting = [
-            ['id' => 1, 'name_th' => 'จำนวนเตียง', 'name' => 'bed_qty', 'value' => ''],
-            ['id' => 2, 'name_th' => 'Token Authen Kiosk สปสช.', 'name' => 'token_authen_kiosk_nhso', 'value' => ''],
-            ['id' => 3, 'name_th' => 'Telegram Token', 'name' => 'telegram_token', 'value' => ''],
-            ['id' => 4, 'name_th' => 'Telegram Chat ID Notify_Summary', 'name' => 'telegram_chat_id', 'value' => ''], 
-            ['id' => 5, 'name_th' => 'ค่า K ', 'name' => 'k_value', 'value' => '1'],   
-            ['id' => 6, 'name_th' => 'Base Rate UCS ในเขต', 'name' => 'base_rate', 'value' => '8350'],
-            ['id' => 7, 'name_th' => 'Base Rate UCS นอกเขต', 'name' => 'base_rate2', 'value' => '9600'],  
-            ['id' => 8, 'name_th' => 'Base Rate OFC', 'name' => 'base_rate_ofc', 'value' => '6200'],  
-            ['id' => 9, 'name_th' => 'Base Rate LGO', 'name' => 'base_rate_lgo', 'value' => '6194'],  
-            ['id' => 10, 'name_th' => 'Base Rate SSS', 'name' => 'base_rate_sss', 'value' => '6200'],
-        ];
-        
-        foreach ($main_setting as $row) {
-            $check = MainSetting::where('id', $row['id'])->count();
-            if ($check > 0) {
-                DB::table('main_setting')
-                ->where('id', $row['id']) 
-                ->update([
-                    'name_th' => $row['name_th'],
-                ]);
-            } else {
-                DB::table('main_setting')
-                ->insert([
-                    'id' => $row['id'],
-                    'name_th' => $row['name_th'],
-                    'name' => $row['name'],
-                    'value' => $row['value'],
-                ]);
-            }
-        }
-    //Table lookup_icode-----------------------------------------------------------------------------------------------------------
-        $table = 'lookup_icode';
-        $columnsToAdd = [        
-            ['name' => 'herb32', 'definition' => 'VARCHAR(1) NULL'], 
-            ['name' => 'kidney', 'definition' => 'VARCHAR(1) NULL AFTER `herb32`'],
-            ['name' => 'ems', 'definition' => 'VARCHAR(1) NULL AFTER `kidney`']
-        ];
+        DB::beginTransaction();
 
         try {
-            foreach ($columnsToAdd as $col) {
-                if (!Schema::hasColumn($table, $col['name'])) {
-                    DB::statement("ALTER TABLE $table ADD COLUMN {$col['name']} {$col['definition']}");
+            // 1. อัพเดตหรือเพิ่ม main_setting
+            $main_setting = [
+                ['id' => 1, 'name_th' => 'จำนวนเตียง', 'name' => 'bed_qty', 'value' => ''],
+                ['id' => 2, 'name_th' => 'Token Authen Kiosk สปสช.', 'name' => 'token_authen_kiosk_nhso', 'value' => ''],
+                ['id' => 3, 'name_th' => 'Telegram Token', 'name' => 'telegram_token', 'value' => ''],
+                ['id' => 4, 'name_th' => 'Telegram Chat ID Notify_Summary', 'name' => 'telegram_chat_id', 'value' => ''], 
+                ['id' => 5, 'name_th' => 'ค่า K ', 'name' => 'k_value', 'value' => '1'],   
+                ['id' => 6, 'name_th' => 'Base Rate UCS ในเขต', 'name' => 'base_rate', 'value' => '8350'],
+                ['id' => 7, 'name_th' => 'Base Rate UCS นอกเขต', 'name' => 'base_rate2', 'value' => '9600'],  
+                ['id' => 8, 'name_th' => 'Base Rate OFC', 'name' => 'base_rate_ofc', 'value' => '6200'],  
+                ['id' => 9, 'name_th' => 'Base Rate LGO', 'name' => 'base_rate_lgo', 'value' => '6194'],  
+                ['id' => 10, 'name_th' => 'Base Rate SSS', 'name' => 'base_rate_sss', 'value' => '6200'],
+            ];
+
+            foreach ($main_setting as $row) {
+                $exists = MainSetting::where('id', $row['id'])->exists();
+                if ($exists) {
+                    DB::table('main_setting')
+                        ->where('id', $row['id'])
+                        ->update([
+                            'name_th' => $row['name_th'],
+                        ]);
+                } else {
+                    DB::table('main_setting')
+                        ->insert([
+                            'id' => $row['id'],
+                            'name_th' => $row['name_th'],
+                            'name' => $row['name'],
+                            'value' => $row['value'],
+                        ]);
                 }
             }
+
+            // 2. เพิ่มคอลัมน์ใน lookup_icode
+            $table_lookup = 'lookup_icode';
+            $columnsToAdd_lookup = [        
+                ['name' => 'herb32', 'definition' => 'VARCHAR(1) NULL'], 
+                ['name' => 'kidney', 'definition' => 'VARCHAR(1) NULL AFTER `herb32`'],
+                ['name' => 'ems', 'definition' => 'VARCHAR(1) NULL AFTER `kidney`']
+            ];
+
+            if (Schema::hasTable($table_lookup)) {
+                foreach ($columnsToAdd_lookup as $col) {
+                    if (!Schema::hasColumn($table_lookup, $col['name'])) {
+                        DB::statement("ALTER TABLE $table_lookup ADD COLUMN {$col['name']} {$col['definition']}");
+                    }
+                }
+            } 
+
+            // 3.... 
+            
+            DB::commit();
 
             return redirect()->route('admin.main_setting')->with('success', 'Upgrade Structure สำเร็จ');
 
         } catch (\Exception $e) {
+            DB::rollBack();
             return back()->with('error', 'เกิดข้อผิดพลาด: ' . $e->getMessage());
         }
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,15 +23,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         Paginator::useBootstrapFive();
-          
-        // ตรวจสอบว่ามีข้อมูลในตาราง lookup_icode มีรายการฟอกไต หรือไม่
-        $hasLookupIcode_kidney = DB::table('lookup_icode')
-            ->where('kidney', '<>', '')
-            ->exists();
+        //  Paginator::useBootstrapFive();
 
-        // แชร์ตัวแปรนี้ไปยังทุก view
-        View::share('hasLookupIcode_kidney', $hasLookupIcode_kidney);
+        // // ตรวจสอบว่ามีข้อมูลในตาราง lookup_icode มีรายการฟอกไต หรือไม่
+        // $hasLookupIcode_kidney = DB::table('lookup_icode')
+        //     ->where('kidney', '<>', '')
+        //     ->exists();
+
+        // // แชร์ตัวแปรนี้ไปยังทุก view
+        // View::share('hasLookupIcode_kidney', $hasLookupIcode_kidney);
+        //----------------------------------------------------------------------------------------
+
+        View::composer('*', function ($view) {
+            $hasLookupIcode_kidney = false;
+
+            // เฉพาะหน้าที่ยกเว้น ไม่โหลดจาก DB แต่ยังแชร์ค่าดีฟอลต์
+            if (!request()->is('admin/main_setting')) {
+                if (Schema::hasColumn('lookup_icode', 'kidney')) {
+                    $hasLookupIcode_kidney = DB::table('lookup_icode')
+                        ->where('kidney', '<>', '')
+                        ->exists();
+                }
+            }
+            $view->with('hasLookupIcode_kidney', $hasLookupIcode_kidney);
+        });
+
     }
 
 }

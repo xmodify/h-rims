99e38c4e3f26beac0e635e2483646c494be984a9
xmodify <xmodifya@gmail.com>
2025-10-15 14:18:57 +0700

    update

--- app/Http/Controllers/ClaimOpController.php
+++ app/Http/Controllers/ClaimOpController.php
@@ -938,9 +938,10 @@ class ClaimOpController extends Controller
             INNER JOIN hrims.lookup_icode li ON op.icode = li.icode
                 WHERE op.vstdate BETWEEN ? AND ? AND li.ppfs = "Y" GROUP BY op.vn) o2 ON o2.vn=o.vn
             LEFT JOIN hrims.nhso_endpoint ep ON ep.cid=v.cid AND ep.vstdate=o.vstdate AND ep.claimCode LIKE "EP%"
+            LEFT JOIN hrims.stm_ofc stm ON stm.cid=pt.cid AND stm.vstdate = o.vstdate AND LEFT(stm.vsttime,5) =LEFT(o.vsttime,5)
             WHERE (o.an ="" OR o.an IS NULL) AND p.hipdata_code = "OFC" AND o.vstdate BETWEEN ? AND ?
             AND p.pttype NOT IN ('.$pttype_checkup.')
-            AND v.income <>"0" AND kidney.vn IS NULL AND oe.upload_datetime IS NULL 
+            AND v.income <>"0" AND kidney.vn IS NULL AND oe.upload_datetime IS NULL AND stm.cid IS NULL
             GROUP BY o.vn ORDER BY o.vstdate,o.vsttime',[$start_date,$end_date,$start_date,$end_date]);
 
         $claim=DB::connection('hosxp')->select('
@@ -971,7 +972,7 @@ class ClaimOpController extends Controller
             LEFT JOIN hrims.stm_ucs stm_uc ON stm_uc.cid=pt.cid AND stm_uc.vstdate = o.vstdate AND LEFT(stm_uc.vsttime,5) =LEFT(o.vsttime,5)
             WHERE (o.an ="" OR o.an IS NULL) AND p.hipdata_code = "OFC" AND o.vstdate BETWEEN ? AND ?
             AND p.pttype NOT IN ('.$pttype_checkup.')
-            AND v.income <>"0" AND kidney.vn IS NULL AND oe.upload_datetime IS NOT NULL
+            AND v.income <>"0" AND kidney.vn IS NULL AND (oe.upload_datetime IS NOT NULL OR stm.cid IS NOT NULL)
             GROUP BY o.vn ORDER BY o.vstdate,o.vsttime',[$start_date,$end_date,$start_date,$end_date]);
 
         return view('claim_op.ofc',compact('budget_year_select','budget_year','start_date','end_date','search','claim','month','claim_price','receive_total'));

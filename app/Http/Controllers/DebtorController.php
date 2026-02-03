fa2b8129191d8e02fb27da6af4b97f6aa889a938
xmodify <xmodifya@gmail.com>
2026-02-03 14:54:54 +0700

    update

--- app/Http/Controllers/DebtorController.php
+++ app/Http/Controllers/DebtorController.php
@@ -122,13 +122,12 @@ class DebtorController extends Controller
                 SUM(IFNULL(v.income,0)) - SUM(IFNULL(rc.rcpt_money,0)) - SUM(IFNULL(pp.ppfs_price,0)) AS debtor
             FROM ovst o
             LEFT JOIN ipt i ON i.vn = o.vn
-            LEFT JOIN vn_stat v ON v.vn = o.vn
-            LEFT JOIN visit_pttype vp ON vp.vn = o.vn
-            LEFT JOIN pttype p ON p.pttype = vp.pttype
+            LEFT JOIN vn_stat v ON v.vn = o.vn            
+            LEFT JOIN pttype p ON p.pttype = v.pttype
             LEFT JOIN ( SELECT r.vn,SUM(r.bill_amount) AS rcpt_money 
                     FROM rcpt_print r
                     WHERE NOT EXISTS (SELECT 1 FROM rcpt_abort a WHERE a.rcpno = r.rcpno)
-                    GROUP BY r.vn) rc ON rc.vn = v.vn
+                    GROUP BY r.vn) rc ON rc.vn = o.vn
             LEFT JOIN (SELECT op.vn,SUM(op.sum_price) AS ppfs_price
                 FROM opitemrece op
                 INNER JOIN hrims.lookup_icode li ON li.icode = op.icode AND li.ppfs = "Y" WHERE op.vstdate BETWEEN ? AND ?

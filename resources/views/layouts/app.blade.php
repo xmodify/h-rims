e0cf4834bde850ba18cf2855771624eb928f4a43
xmodify <xmodifya@gmail.com>
2026-02-05 16:01:52 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -478,7 +478,7 @@
                     <ul class="navbar-nav ms-auto">
                         <li > 
                             <div class="btn text-info">
-                                V. 69-02-05 14:30
+                                V. 69-02-05 16:00
                             </div>   
                         </li>                         
                         <!-- Authentication Links -->

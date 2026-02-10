4d64017cee63627f8e561f36e402f38160899bb7
xmodify <xmodifya@gmail.com>
2026-02-11 01:18:37 +0700

    Fix proxy trust and Vite base path for asset visibility

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-      base: '/h-rims/', 
-      plugins: [
+    base: '/rims/',
+    plugins: [
         laravel({
             input: [
                 'resources/sass/app.scss',

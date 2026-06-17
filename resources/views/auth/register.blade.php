7996e55f03a0aa632b5e049740032ef036fdee5a
xmodify <xmodifya@gmail.com>
2026-06-17 22:26:24 +0700

    fix favicon paths and add shortcut icon tags for better compatibility

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('/images/favicon_darkgreen.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/favicon_darkgreen.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('images/favicon_darkgreen.ico') }}" type="image/x-icon">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">

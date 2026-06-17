739186a350a8a801d58ea52d7208d014e4593a57
xmodify <xmodifya@gmail.com>
2026-06-17 22:47:02 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1024,7 +1024,7 @@
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item d-flex align-items-center me-2">
                             <div class="nav-version-badge">
-                                V.69-06-17 21:00
+                                V.69-06-17 22:00
                             </div>
                         </li>
                         <!-- Authentication Links -->

809ba2651b231858a318cdab60f1991fe13a73d4
xmodify <xmodifya@gmail.com>
2026-07-13 09:51:26 +0700

    Clean scratch scripts

--- scratch/find_all_methods.php
+++
@@ -1,8 +0,0 @@
-<?php
-$content = file_get_contents('app/Http/Controllers/ClaimOpController.php');
-$lines = explode("\n", $content);
-foreach ($lines as $idx => $line) {
-    if (preg_match('/public function ([a-zA-Z0-9_]+)/', $line, $matches)) {
-        echo "Line " . ($idx + 1) . ": " . $matches[1] . "\n";
-    }
-}

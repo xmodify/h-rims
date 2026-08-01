7979b36fe308989a68388535c2ae415097072d50
xmodify <xmodifya@gmail.com>
2026-08-01 10:50:00 +0700

    import DB and Auth facades in NhsoCheckRightController

--- app/Http/Controllers/Api/NhsoCheckRightController.php
+++ app/Http/Controllers/Api/NhsoCheckRightController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Services\LicenseVerificationService;
 
 class NhsoCheckRightController extends Controller

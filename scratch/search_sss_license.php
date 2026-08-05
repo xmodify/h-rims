d5fc4c71896e6d742a63c44e8ef66f154834f1ce
xmodify <xmodifya@gmail.com>
2026-08-05 18:21:29 +0700

    clean scratch scripts

--- scratch/search_sss_license.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'd:/Project Laravel/h-rims/app/Http/Controllers/SssExportController.php';
-$content = file_get_contents($file);
-$lines = explode("\n", $content);
-foreach ($lines as $i => $line) {
-    if (strpos($line, 'license') !== false) {
-        echo ($i + 1) . ": " . trim($line) . "\n";
-    }
-}

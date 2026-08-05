9729827ba4d1e0a001dd0d5d98e184321cd1dbf9
xmodify <xmodifya@gmail.com>
2026-08-05 18:17:55 +0700

    clean scratch scripts

--- scratch/search_csop.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'd:/Project Laravel/h-rims/app/Http/Controllers/CsopExportController.php';
-$content = file_get_contents($file);
-$lines = explode("\n", $content);
-foreach ($lines as $i => $line) {
-    if (strpos($line, 'doctor') !== false || strpos($line, 'license') !== false) {
-        echo ($i + 1) . ": " . trim($line) . "\n";
-    }
-}

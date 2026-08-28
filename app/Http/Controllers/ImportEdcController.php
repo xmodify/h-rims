1de2884be13f080e3ab81a01eb0de2c6c498dedf
xmodify <xmodifya@gmail.com>
2026-08-28 15:26:05 +0700

    enhance HOSxP ovst_seq and visit_pttype EDC writeback on import

--- app/Http/Controllers/ImportEdcController.php
+++ app/Http/Controllers/ImportEdcController.php
@@ -506,25 +506,34 @@ class ImportEdcController extends Controller
                     $importedCount++;
                 }
 
-                // Sync/Update HOSxP connection (ovst_seq.edc_approve_list_text)
+                // Sync/Update HOSxP connection (ovst_seq.edc_approve_list_text & visit_pttype.claim_code)
                 if (!empty($cid) && !empty($vstdate) && !empty($finalCode)) {
                     try {
-                        $vn = DB::connection('hosxp')->table('ovst')
+                        $vns = DB::connection('hosxp')->table('ovst')
                             ->join('patient', 'patient.hn', '=', 'ovst.hn')
                             ->where('patient.cid', $cid)
                             ->where('ovst.vstdate', $vstdate)
-                            ->value('ovst.vn');
+                            ->pluck('ovst.vn');
 
-                        if ($vn) {
+                        foreach ($vns as $vn) {
                             DB::connection('hosxp')->table('ovst_seq')
+                                ->updateOrInsert(
+                                    ['vn' => $vn],
+                                    ['edc_approve_list_text' => $finalCode]
+                                );
+
+                            DB::connection('hosxp')->table('visit_pttype')
                                 ->where('vn', $vn)
+                                ->where(function($q) {
+                                    $q->whereNull('claim_code')->orWhere('claim_code', '');
+                                })
                                 ->update([
-                                    'edc_approve_list_text' => $finalCode
+                                    'claim_code' => $finalCode
                                 ]);
                             $matchedCount++;
                         }
                     } catch (\Throwable $ex) {
-                        Log::warning("Could not update HOSxP ovst_seq: " . $ex->getMessage());
+                        Log::warning("Could not update HOSxP ovst_seq / visit_pttype: " . $ex->getMessage());
                     }
                 }
             }
